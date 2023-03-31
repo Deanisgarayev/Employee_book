d609bd1857f4344e9b454974942028546a3d56cd
Денис <garaevdenis2019@gmail.com>
2023-03-31 20:40:58 +0500

    initial commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,15 +1,17 @@
 public class Employee {
    private String fullName;
     private int salary;
-    private String electrician;
-    private String stonemason;
-    private String plumber;
-    private String welder;
-    private String carpenter;
+    private String department;
+//    private String electrician;
+//    private String stonemason;
+//    private String plumber;
+//    private String welder;
+//    private String carpenter;
 
-    public Employee(String fullName, int salary) {
+    public Employee(String fullName, int salary, String department) {
         this.fullName = fullName;
         this.salary = salary;
+        this.department =department;
     }
 
     public String getFullName() {
@@ -20,8 +22,19 @@ public class Employee {
         return this.salary;
     }
 
-
+    public String getDepartment() {
+        return this.department;
+    }
     public void setSalary(int salary) {
         this.salary = salary;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя " + fullName + " Отдел " + department + " Зарплата " + salary;
     }
 }
